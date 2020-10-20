04b8b29490c6a1055175e7b099d91309a2857696
ajendrasaxena <ajendrasaxena24@gmail.com>
2020-10-20 11:16:27 +0530

    First commit

---
+++ Testing/src/javaapplication43/Train_results.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication43;
+
+import java.sql.Date;
+import java.util.Vector;
+
+/**
+ *
+ * @author ajendrasaxena
+ */
+public class Train_results {
+    String train,name,board,dest;
+    Vector<String> coaches=new Vector<String>();
+    Vector<String> dates=new Vector<String>();
+}
